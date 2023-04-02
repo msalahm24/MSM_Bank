195617769a8c1e183e920093b03f67b004acfe72
msalahm <msalahm@tribal.credit>
2023-04-02 17:13:08 +0200

    Adding paseto token

--- util/random.go
+++ util/random.go
@@ -7,45 +7,46 @@ import (
 	"time"
 )
 
-const(
+const (
 	alphabets = "qwertyuiopasdfghjklzxcvbnm"
 )
-func init(){
+
+func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
 // Generate random int between min and max
-func RandomInt(min,max int64)int64{
-	return min + rand.Int63n(max - min + 1)
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
 }
 
 // Generate Random string
-func RandomString(n int)string{
-  var sb strings.Builder
+func RandomString(n int) string {
+	var sb strings.Builder
 
-  k := len(alphabets)
+	k := len(alphabets)
 
-  for i := 0 ; i <= n ; i++ {
-	c := alphabets[rand.Intn(k)]
-	sb.WriteByte(c)
-  }
-  return sb.String()
+	for i := 0; i < n; i++ {
+		c := alphabets[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
 }
 
-func RandomOwnerName()string{
+func RandomOwnerName() string {
 	return RandomString(6)
 }
 
-func RandomMoney()int64{
-	return RandomInt(0,100000)
+func RandomMoney() int64 {
+	return RandomInt(0, 100000)
 }
 
-func RandomCurrency()string{
-	currencies := []string{"USD","EUR","CAD"}
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
 
-func RandomEmail()string{
-	return fmt.Sprintf("%s@email.com",RandomString(10))
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(10))
 }
