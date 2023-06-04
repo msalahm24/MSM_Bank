650c79f22159985fc7430fb08ef92cafe2c4227d
msalahm <msalahm@tribal.credit>
2023-06-04 16:31:17 +0300

    adding grpc gateway to serve poth the api call and grpc call

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/mahmoud24598salah/MSM_Bank/api"
 	db "github.com/mahmoud24598salah/MSM_Bank/db/sqlc"
@@ -26,7 +29,7 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
-
+	go runGatewayServer(config,store)
 	runGrpcServer(config,store)
 
 }
@@ -49,6 +52,34 @@ func runGrpcServer(config util.Config, store db.Store){
 	err = grpcServer.Serve(listener)
 	if  err != nil{
 		log.Fatal("can not start gRPC server", err)
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store){
+	server,err := gapi.Newserver(config,store)
+	if err != nil{
+		log.Fatal("Cannot create gRPC server", err)
+	} 
+
+	grpcmux := runtime.NewServeMux()
+	ctx,cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err = pb.RegisterMSMBankHandlerServer(ctx,grpcmux,server)
+	if err != nil{
+		log.Fatal("can not register handler server ")
+	}
+
+	mux :=http.NewServeMux()
+	mux.Handle("/",grpcmux)
+	listener, err := net.Listen("tcp",config.HTTPSreverAddress)
+
+	if err != nil{
+		log.Fatal("can not create listener: ",err)
+	}
+	log.Printf("start http gateway server at %s",listener.Addr().String())
+	err = http.Serve(listener,mux)
+	if  err != nil{
+		log.Fatal("can not start http gateway server", err)
 	}
 }
 
